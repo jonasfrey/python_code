6ed561cce4d95ecebf88b4995f2ff3c253553343
jonas <jonas.immanuel.frey@gmail.com>
2020-09-16 19:50:46 +0000

    Update snake.py

---
+++ snake.py
@@ -0,0 +1,12 @@
+class Canvas:
+    def __init__():
+        self.width = 100
+        self.height = 100
+
+    def clear(self):
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+
+
+
+print("asdfasdf")
