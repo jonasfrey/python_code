8603b7eae6e69193b54e99ac582a5c522386511f
jf18j492 <jonas.frey@csh.unibe.ch>
2021-10-15 02:02:28 +0200

    step by step gui is getting dynamic

--- pyqt5_gui.py
+++ pyqt5_gui.py
@@ -4,9 +4,10 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import forcepoint, pyqtSignal, pyqtSlot, Qt, QThread
 import json
 import sys
+import random
 from json.encoder import py_encode_basestring_ascii
 
-from cv2 import CAP_PROP_INTELPERC_DEPTH_FOCAL_LENGTH_VERT
+from cv2 import CAP_PROP_INTELPERC_DEPTH_FOCAL_LENGTH_VERT, THRESH_TOZERO
 
 
 class Data:
@@ -16,38 +17,92 @@ class Data:
 class Pyqt5_app(QWidget):
     def __init__(self):
 
-        self.qApplication = QApplication(sys.argv)
         super().__init__()
 
+        self.setup_gui()
+
+    def setup_gui(self):
+
+        self.setWindowTitle('asdf')
+
+        self.main_layout = QHBoxLayout()
+        
+
+        pyqt5_layout_object = Pyqt5_layout_object('QVBoxLayout')
+
+        self.left_box = QVBoxLayout()
+        self.left_box = pyqt5_layout_object.pyqt5_class_instance
+        label = QLabel('111')
+        self.left_box.addWidget(label)
+        self.main_layout.addLayout(self.left_box)
+
+        self.right_box = QHBoxLayout()
+        label = QLabel('222')
+        self.right_box.addWidget(label)
+        self.main_layout.addLayout(self.right_box)
+            
+
+
         self.pyqt5_layout = Pyqt5_layout()
+        self.setLayout(self.pyqt5_layout.converted_layout.pyqt5_class_instance)
+        #self.setLayout(self.main_layout)
         
-        self.main_layout = QVBoxLayout()
-
-        # self.setLayout(self.pyqt5_layout.converted_layout.pyqt5_class_instance)
-        self.setLayout(self.main_layout)
         
-        self.disply_width = 888
-        self.display_height = 500
-
         self.show()
+        self.setGeometry(300, 300, 300, 220)
+        # self.pyqt5_layout.converted_layout.pyqt5_class_instance.addWidget(label)
 
-        self.data = Data()
+        # self.pyqt5_layout = Pyqt5_layout()
+        
+        #  self.setLayout(self.pyqt5_layout.converted_layout.pyqt5_class_instance)
+
+        # self.data = Data()
+
+        # w.setWindowTitle('Simple')
+        # w.show()
+
+
+        
         
 
 
 class Pyqt5_layout_object():
-    def __init__(self, pyqt5_class_name, parent_pyqt5_layout_object):
+    """
+    alias:pyqt5 class name
+    """
+    qbox_layout_class_name_mappings = {
+        'column':'QHBoxLayout',
+        'row':'QVBoxLayout'
+    }
+
+    def __init__(self, string):
         self.c = []
-        klass = globals()[pyqt5_class_name]
-        self.pyqt5_class_instance = klass()
-        label = QLabel('Webcams')
-        self.pyqt5_class_instance.addWidget(label)
-        self.parent_pyqt5_layout_object = parent_pyqt5_layout_object
+        self.get_pyqt5_class_name_by_string(string)
+        pyqt5_class_object = globals()[self.get_pyqt5_class_name_by_string(string)]
+        pyqt5_class_object_instance = pyqt5_class_object()
+
+        qw = QWidget()
+        r = lambda: random.randint(0,255)
+        rc = ('#%02X%02X%02X' % (r(),r(),r()))
+        label = QLabel(rc)
+        qw.setStyleSheet('background-color: '+rc)
+        pyqt5_class_object_instance.addWidget(qw)
+        pyqt5_class_object_instance_inner = pyqt5_class_object()
+        qw.setLayout(pyqt5_class_object_instance_inner)
+        pyqt5_class_object_instance_inner.addWidget(label)
+        pyqt5_class_object_instance.addLayout(pyqt5_class_object_instance_inner)
+        
+        self.pyqt5_class_instance = pyqt5_class_object_instance
+
+
+        # label = QLabel('Webcams')
+        # self.pyqt5_class_instance.addWidget(label)
     
-    @property
-    def addLayout(self, layout):
-        print('test test test')
-        return self.pyqt5_class_instance.addLayout(layout)
+    def get_pyqt5_class_name_by_string(self, string):
+        try:
+            return Pyqt5_layout_object.qbox_layout_class_name_mappings[string]
+        except:
+            return string
 
 class Pyqt5_layout:
 
@@ -56,16 +111,56 @@ class Pyqt5_layout:
         self.converted_layout = None
         self.layout_json = """
             {
-                "pyqt5_class_name":"QHBoxLayout",
+                "pyqt5_class_name":"column",
                 "c":[
-                    {
-                        "pyqt5_class_name":"QVBoxLayout",
-                        "c":[
-                            {
-                                "pyqt5_class_name":"QHBoxLayout"
-                            }
-                        ]
-                    }
+                        {
+                            "pyqt5_class_name":"row",
+                            "c":[
+                                    {
+                                        "pyqt5_class_name":"column",
+                                        "c":[
+                                                {
+                                                    "pyqt5_class_name":"row",
+                                                    "c":[
+                                                        
+                                                    ]
+                                                }
+                                        ]
+                                    }
+                            ]
+                        },
+                        {
+                            "pyqt5_class_name":"row",
+                            "c":[
+                                    {
+                                        "pyqt5_class_name":"column",
+                                        "c":[
+                                                {
+                                                    "pyqt5_class_name":"row",
+                                                    "c":[
+                                                        {
+                                                            "pyqt5_class_name":"column",
+                                                            "c":[
+                                                                {
+                                                                    "pyqt5_class_name":"row"
+                                                                },
+                                                                {
+                                                                    "pyqt5_class_name":"row"
+                                                                },
+                                                                {
+                                                                    "pyqt5_class_name":"row"
+                                                                },
+                                                                {
+                                                                    "pyqt5_class_name":"row"
+                                                                }
+                                                            ]
+                                                        }
+                                                    ]
+                                                }
+                                        ]
+                                    }
+                            ]
+                        }
                 ]
             }
         """
@@ -85,13 +180,18 @@ class Pyqt5_layout:
         # print(obj)
 
     def foreach_prop_in_oject(self, object, parent):
-        converted_object = self.get_pyqt5_layout_object_by_json_object(object, parent)
+
+        converted_object = Pyqt5_layout_object(object['pyqt5_class_name'])
         # print(object['c'])
 
         if('c' in object):
             for obj in object['c']:
                 pyqt5_layout_object = self.foreach_prop_in_oject(obj, converted_object)
-                pyqt5_layout_object.pyqt5_class_instance.addLayout(converted_object.pyqt5_class_instance)
+                try:
+                    converted_object.pyqt5_class_instance.addLayout(pyqt5_layout_object.pyqt5_class_instance)
+                except:
+                    print('alreadz')
+                    raise
                 converted_object.c.append(pyqt5_layout_object)
 
 
@@ -99,12 +199,13 @@ class Pyqt5_layout:
         
             # print(type(value))
 
-    def get_pyqt5_layout_object_by_json_object(self, json_object, parent=QVBoxLayout()):
-        # print(json_object.p)
-        # print(vars(json_object))
-        return Pyqt5_layout_object(json_object['pyqt5_class_name'], parent)
 
 
 
+if __name__ == '__main__':
 
-pyqt5_app = Pyqt5_app()
+    qApplication = QApplication(sys.argv)
+
+    pyqt5_app = Pyqt5_app()
+
+    sys.exit(qApplication.exec_())
